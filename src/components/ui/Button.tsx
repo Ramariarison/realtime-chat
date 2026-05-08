042b8eb5ec2d21fd4542d8e3b08ed1a8513e7857
Ramariarison <andryramariarison@gmail.com>
2026-05-08 20:00:17 +0300

    refactor(components): amélioration de la gestion des props des composants Input et Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,14 @@
 type Props = {
-  children: React.ReactNode
-  onClick?: () => void
+  children: React.ReactNode,
+  onClick?: () => void,
+  type: "submit"
 }
 
-export default function Button({ children, onClick }: Props) {
+export default function Button({ children, onClick, type }: Props) {
   return (
     <button
       onClick={onClick}
+      type={type}
       className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-lg transition"
     >
       {children}
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,13 +1,19 @@
 type Props = {
-  type: string
-  placeholder: string
+  type: string,
+  name_input: string,
+  placeholder: string,
+  value: string,
+  change: React.ChangeEventHandler<HTMLInputElement>,
 }
 
-export default function Input({ type, placeholder }: Props) {
+export default function Input({ type, placeholder, value, change, name_input }: Props) {
   return (
     <input
       type={type}
+      name={name_input}
       placeholder={placeholder}
+      value={value}
+      onChange={change}
       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
     />
   )
