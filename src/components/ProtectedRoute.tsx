40dcdc5563285dd2d06b0e981305d0bedd08ef0c
Ramariarison <andryramariarison@gmail.com>
2026-05-12 22:59:27 +0300

    feat: implémentation de la protection des routes par rôles

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+type ProtectedRouteProps = {
+  allowedRoles: string[];
+};
+
+export default function ProtectedRoute({ allowedRoles }: ProtectedRouteProps) {
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("user_role");
+
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+
+  if (!allowedRoles.includes(role || "")) {
+
+    if (role === 'admin') return <Navigate to="/admin/dashboard" replace />;
+
+    return <Navigate to="/user/chat" replace />;
+  }
+
+  return <Outlet />;
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import MainLayout from "../layout/MainLayout"
 import AdminLayout from "../layout/AdminLayout"
@@ -8,32 +8,33 @@ import AuthPage from "../pages/AuthPage"
 import ChatPage from "../pages/ChatPage"
 import Profilepage from "../pages/ProfilePage"
 import UserList from "../components/admin/UserList"
+import ProtectedRoute from "../components/ProtectedRoute"
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-
-
+        {/* Zone Publique */}
         <Route path="/" element={<AuthPage />} />
 
-
-        <Route element={<MainLayout />}>
-        
-          <Route path="/user/chat" element={<ChatPage />} />
-
-          <Route path="/user/profile" element={<Profilepage />} />
-
+        {/* Utilisateur Normal */}
+        <Route element={<ProtectedRoute allowedRoles={['normal']} />}>
+          <Route element={<MainLayout />}>
+            <Route path="/user/chat" element={<ChatPage />} />
+            <Route path="/user/profile" element={<Profilepage />} />
+          </Route>
         </Route>
 
-        <Route element={<AdminLayout />}>
-        
-          <Route path="/admin/users" element={<UserList />}/>
-
-          <Route path="/admin/dashboard" element/>
-
+        {/* Admin */}
+        <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
+          <Route element={<AdminLayout />}>
+            <Route path="/admin/users" element={<UserList />} />
+            <Route path="/admin/dashboard" element={<div>Dashboard Admin</div>} />
+          </Route>
         </Route>
 
+        {/* Si la route n'existe pas */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
